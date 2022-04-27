e793685f63a4eead71f8771a5cc6f2d134f276da
afahgan <afahgan@students.iugaza.edu.ps>
2022-04-27 12:13:33 +0300

    fs: update index.html - app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 import React from 'react';
 function App() {
   return (
-    <div className="App">
-     Fitness House
+    <div className="container">
+      <h1>Hello World</h1>
     </div>
   );
 }
