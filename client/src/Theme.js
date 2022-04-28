3c9c49e8da6ac0324547ed1a73da9dac11b2ce28
rawaneyad <rawaneyad20@gmail.com>
2022-04-28 16:43:11 +0300

    fix: solve eslint error

--- client/src/Theme.js
+++ client/src/Theme.js
@@ -1,27 +1,27 @@
-import { createTheme, ThemeProvider } from '@mui/material';
-import rtlPlugin from 'stylis-plugin-rtl';
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
-import PropTypes from 'prop-types';
+import { createTheme, ThemeProvider } from "@mui/material";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import PropTypes from "prop-types";
 
 // Create rtl cache
 const cacheRtl = createCache({
-  key: 'muirtl',
+  key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
 });
 
 const theme = createTheme({
-  direction: 'rtl',
+  direction: "rtl",
   typography: {
-    fontFamily: ['Cairo', 'sans-serif'].join(','),
+    fontFamily: ["Cairo", "sans-serif"].join(","),
   },
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#276678',
-      dark: '#E5E5E5',
-      contrastText: '#fff',
+      light: "#757ce8",
+      main: "#276678",
+      dark: "#E5E5E5",
+      contrastText: "#fff",
     },
   },
 });
@@ -29,9 +29,7 @@ const theme = createTheme({
 function Theme({ children }) {
   return (
     <ThemeProvider theme={theme}>
-      <CacheProvider value={cacheRtl}>
-        {children}
-      </CacheProvider>
+      <CacheProvider value={cacheRtl}>{children}</CacheProvider>
     </ThemeProvider>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import Theme from './Theme';
+import Theme from "./Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Theme>
-    <App />
+      <App />
     </Theme>
   </Router>
 );
