672a225c4d8bbfcc68b317f824444f73696f1553
rawaneyad <rawaneyad20@gmail.com>
2022-04-29 00:37:43 +0300

    db: update index file

--- server/database/models/index.ts
+++ server/database/models/index.ts
@@ -1,3 +1,15 @@
-import sequelize from '../config/connection';
+import Subscription from './subscription';
+import Reviews from './reviews';
 
-export default sequelize;
+import Gym from './gyms';
+import User from './user';
+
+Subscription.belongsTo(User);
+Subscription.belongsTo(Gym);
+
+Reviews.belongsTo(User);
+Reviews.belongsTo(Gym);
+
+export {
+  Subscription, Reviews,
+};
