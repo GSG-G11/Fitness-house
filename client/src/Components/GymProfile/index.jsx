ff9b59eefbd29fb6bb5ef480dd36af92ac07ad5e
afahgan <afahgan@students.iugaza.edu.ps>
2022-05-09 13:31:05 +0300

    style: fix style

--- client/src/Components/GymProfile/index.jsx
+++ client/src/Components/GymProfile/index.jsx
@@ -26,54 +26,56 @@ function GymProfile({ gymData }) {
       <Divider variant="middle" />
 
       <div className="leftside">
-        <Box sx={{ p: 2, display: "flex", justifyContent: "space-between" }}>
-          <Box sx={{ display: "flex", gap: "0.5rem" }}>
-            <Avatar alt="card" src={logo} />
-            <Stack spacing={0.5}>
-              <Typography className="gymCard__name" fontWeight={700} noWrap>
-                {gymName}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {city}
-              </Typography>
-            </Stack>
-          </Box>
+        <div className="topsec">
+          <Box sx={{ p: 2, display: "flex", justifyContent: "space-between" }}>
+            <Box sx={{ display: "flex", gap: "0.5rem" }}>
+              <Avatar alt="card" src={logo} />
+              <Stack spacing={0.5}>
+                <Typography className="gymCard__name" fontWeight={700} noWrap>
+                  {gymName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {city}
+                </Typography>
+              </Stack>
+            </Box>
 
-          <Box
-            sx={{
-              marginLeft: 0,
-              position: "relative",
-              display: "inline-flex",
-            }}
-          >
-            <CircularProgress variant="determinate" value={progress} />
             <Box
               sx={{
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0,
-                height: 40,
-                position: "absolute",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
+                marginLeft: 0,
+                position: "relative",
+                display: "inline-flex",
               }}
             >
-              <Typography
-                variant="caption"
-                component="div"
-                color="text.secondary"
+              <CircularProgress variant="determinate" value={progress} />
+              <Box
+                sx={{
+                  top: 0,
+                  left: 0,
+                  bottom: 0,
+                  right: 0,
+                  height: 40,
+                  position: "absolute",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
               >
-                {progress}%
-              </Typography>
+                <Typography
+                  variant="caption"
+                  component="div"
+                  color="text.secondary"
+                >
+                  {progress}%
+                </Typography>
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </div>
 
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            {description}
+            <p className="description"> {description}</p>
           </Typography>
         </CardContent>
         <Divider variant="middle" />
