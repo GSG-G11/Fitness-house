49345a12af5a22e30129fe25ce1098f37f62d237
rawaneyad <rawaneyad20@gmail.com>
2022-05-09 10:40:54 +0300

    fix: edit tests

--- test/routes.test.ts
+++ test/routes.test.ts
@@ -12,24 +12,36 @@ describe("Gyms API", () => {
   });
   test('Gyms - GET - /api/v1/gyms/:id - ', async () => {
     const response = await request(app).get('/api/v1/gyms/1').expect(200);
+    expect(response.body.GymData.gymName).toBe("نادي فريندز للياقة البدنية");
+    expect(response.body.GymData.city).toBe("غزة");
   });
   test('Gyms - GET - /api/v1/gyms/:id - ', async () => {
     const response = await request(app).get('/api/v1/gyms/2').expect(200);
+    expect(response.body.GymData.gymName).toBe("Technogym gaza تكنو جيم");
+    expect(response.body.GymData.city).toBe("غزة");
   });
   test('Gyms - GET - /api/v1/gyms/:id - ', async () => {
     const response = await request(app).get('/api/v1/gyms/3').expect(200);
+    expect(response.body.GymData.gymName).toBe("اوكسجن جيم");
+    expect(response.body.GymData.city).toBe("رفح");
   });
   test('Gyms - GET - /api/v1/gyms/:id - ', async () => {
     const response = await request(app).get('/api/v1/gyms/4').expect(200);
+    expect(response.body.GymData.gymName).toBe("مركز الملكة الأنيقة للسيدات");
+    expect(response.body.GymData.city).toBe("غزة");
   });
   test('Gyms - GET - /api/v1/gyms/:id - ', async () => {
     const response = await request(app).get('/api/v1/gyms/5').expect(200);
+    expect(response.body.GymData.gymName).toBe("Jalaa Gym -جيم نادي الجلاء");
+    expect(response.body.GymData.city).toBe("غزة");
   });
   test('Gyms - GET - /api/v1/gyms/:id - ', async () => {
-    const response = await request(app).get('/api/v1/gyms/50').expect(200);
+    const response = await request(app).get('/api/v1/gyms/50').expect(404);
+    expect(response.body.message).toBe("عذراُ الجيم غير متوفر");
   });
   test('Gyms - GET - /api/v1/gyms/:id - ', async () => {
-    const response = await request(app).get('/api/v1/gyms/gggggggg').expect(500);
+    const response = await request(app).get('/api/v1/gyms/gggggggg').expect(400);
+    expect(response.body.message).toBe("عذراً خطأ في المعرف");
 });
 });
 afterAll(() => {
