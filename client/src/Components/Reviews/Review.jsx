c80c0910d4d2a34bc0f5db538593f31981c75c42
rawaneyad <rawaneyad20@gmail.com>
2022-05-10 12:28:08 +0300

    fix: edit PropTypes and delete div

--- client/src/Components/Reviews/Review.jsx
+++ client/src/Components/Reviews/Review.jsx
@@ -5,23 +5,21 @@ import PropTypes from "prop-types";
 export default function userReview({ review }) {
   const { rate, description, createdAt, username, avatar } = review;
   return (
-    <div>
-      <Box sx={{ display: "flex", gap: "1rem", paddingTop: "1.5rem" }}>
-        <Avatar alt="card" src={avatar} />
-        <Stack spacing={0.5}>
-          <Typography className="user__name" fontWeight={700} noWrap>
-            {username}
-          </Typography>
-          <Rating name="read-only" value={rate} readOnly />
-          <Typography variant="body2">{description}</Typography>
-          <Typography variant="body2" color="text.secondary">
-            {createdAt}
-          </Typography>
-        </Stack>
-      </Box>
-    </div>
+    <Box sx={{ display: "flex", gap: "1rem", paddingTop: "1.5rem" }}>
+      <Avatar alt="card" src={avatar} />
+      <Stack spacing={0.5}>
+        <Typography className="user__name" fontWeight={700} noWrap>
+          {username}
+        </Typography>
+        <Rating name="read-only" value={rate} readOnly />
+        <Typography variant="body2">{description}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          {createdAt}
+        </Typography>
+      </Stack>
+    </Box>
   );
 }
 userReview.propTypes = {
-  userReview: PropTypes.instanceOf(Object).isRequired,
+  review: PropTypes.instanceOf(Object).isRequired,
 };
