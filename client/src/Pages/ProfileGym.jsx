c9fe763bcc30cc0e09cd6d9629ae455bf998568d
rawaneyad <rawaneyad20@gmail.com>
2022-05-11 16:03:39 +0300

    feat: add Loading in ProfileGym page

--- client/src/Pages/ProfileGym.jsx
+++ client/src/Pages/ProfileGym.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { Box, Skeleton, Stack, CardContent } from "@mui/material";
 import { useParams } from "react-router-dom";
 import GymImages from "../Components/GymImages";
-
 import GymReviews from "../Components/Reviews";
-
+import GymCards from "../Components/Cards";
 import GymProfile from "../Components/GymProfile";
 
 import { useGetGymDataQuery } from "../Store/Services/TopGyms";
@@ -13,7 +13,83 @@ export default function ProfileGym() {
   const { data, isLoading, isError, isSuccess } = useGetGymDataQuery(gymId);
   const renderProfile = () => {
     if (isLoading) {
-      return <div> تحميل</div>;
+      return (
+        <Box className="gymprofilecard">
+          <Box className="rightside">
+            <Skeleton
+              sx={{ height: 400, width: 320, borderRadius: "10px" }}
+              animation="wave"
+              variant="rectangular"
+            />
+          </Box>
+          <Box className="leftside">
+            <Box className="topsec">
+              <Box
+                sx={{ p: 2, display: "flex", justifyContent: "space-between" }}
+              >
+                <Box sx={{ display: "flex", gap: "0.5rem" }}>
+                  <Skeleton
+                    animation="wave"
+                    variant="circular"
+                    width={40}
+                    height={40}
+                  />
+                  <Box width={200}>
+                    <Skeleton
+                      animation="wave"
+                      height={10}
+                      style={{ marginBottom: 6 }}
+                    />
+                    <Skeleton animation="wave" height={10} width="80%" />
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+
+            <CardContent>
+              <Skeleton />
+              <Skeleton />
+              <Skeleton width="60%" />
+            </CardContent>
+            <Box sx={{ m: 2 }}>
+              <Stack direction="row" spacing={1}>
+                <Skeleton
+                  animation="wave"
+                  height={50}
+                  width="35%"
+                  style={{ borderRadius: "10px" }}
+                />
+                <Skeleton
+                  animation="wave"
+                  height={50}
+                  width="35%"
+                  style={{ borderRadius: "10px" }}
+                />
+                <Skeleton
+                  animation="wave"
+                  height={50}
+                  width="35%"
+                  style={{ borderRadius: "10px" }}
+                />
+                <Skeleton
+                  animation="wave"
+                  height={50}
+                  width="35%"
+                  style={{ borderRadius: "10px" }}
+                />
+              </Stack>
+            </Box>
+            <Box sx={{ mt: "auto", ml: 2, mb: 2 }}>
+              <Skeleton
+                animation="wave"
+                height={55}
+                width="40%"
+                style={{ borderRadius: "10px" }}
+              />
+            </Box>
+          </Box>
+        </Box>
+      );
     }
     if (isError) {
       return <div>عذرا هناك خطأ , أعد تحديث الصفحة </div>;
@@ -24,18 +100,19 @@ export default function ProfileGym() {
       return <div>النادي غير موجود , أعد تحديث الصفحة </div>;
     return (
       <>
-        <GymProfile gymData={gymData} />
-        <div className="container bg__container imgfooter">
+        <div className="container">
+          <GymProfile gymData={gymData} />
+        </div>
+        <div className="container imgfooter">
           <GymImages gymData={gymData} />
         </div>
-        <GymReviews gymData={gymData} />
+        <div className="container">
+          <GymReviews gymData={gymData} />
+        </div>
+        <GymCards page="ProfileGym" />
       </>
     );
   };
 
-  return (
-    // <div className="container">
-    <>{renderProfile()}</>
-    // </div>
-  );
+  return <>{renderProfile()}</>;
 }
