5ccacd987245648881e1dd8741f685e50eeb3bd7
rawaneyad <rawaneyad20@gmail.com>
2022-05-12 18:22:40 +0300

    feat: create verfiy and sign function

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,5 +1,7 @@
 import CustomError from './CustomError';
+import { checkToken, generateToken } from './jwt';
 import { GymFilter } from './types';
 import { filterValidation, paramsValidation } from './validation';
 
-export { CustomError, GymFilter, filterValidation, paramsValidation };
+export {
+  CustomError, GymFilter, filterValidation, paramsValidation, checkToken, generateToken };
