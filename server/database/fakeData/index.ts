700f93c3cfde072a6c9ae9e5e0dc0c8186c7b5b1
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-05-14 12:53:12 +0300

    db: fix build databse function to create data async

--- server/database/fakeData/index.ts
+++ server/database/fakeData/index.ts
@@ -16,14 +16,31 @@ const { subscription } = subscriptionsJson;
 const builderHandler = async () => {
   await sequelize.sync({ force: true });
 
-  console.log('Database Start seeded ...');
+  console.log('Database Start seeded (user - gym) ...');
 
   await Promise.all([
-    users.map((user: any) => User.create(user)),
-    gyms.map((gym: any) => Gym.create(gym)),
-    images.map((image: any) => Image.create(image)),
-    subscription.map((sub: any) => Subscription.create(sub)),
-    reviews.map((review: any) => Review.create(review)),
+    ...users.map(async (user: any) => {
+      await User.create(user);
+    }),
+    ...gyms.map(async (gym: any) => {
+      await Gym.create(gym);
+    }),
+  ]);
+
+  console.log(
+    'Insert Users and Gyms ~~ Now Database Start seeded (images - subscription - reviews) ...',
+  );
+
+  await Promise.all([
+    ...images.map(async (image: any) => {
+      await Image.create(image);
+    }),
+    ...subscription.map(async (sub: any) => {
+      await Subscription.create(sub);
+    }),
+    ...reviews.map(async (review: any) => {
+      await Review.create(review);
+    }),
   ]);
 
   console.log('Database seeded successfully');
