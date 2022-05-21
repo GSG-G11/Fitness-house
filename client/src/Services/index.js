1e9433dc59d4f32e6183bf8147cf1b33f59cbc05
rawaneyad <rawaneyad20@gmail.com>
2022-05-21 13:20:36 +0300

    fix: fix deploy error

--- client/src/Services/index.js
+++ client/src/Services/index.js
@@ -1,9 +1,12 @@
-import * as Genders from "./genders.json";
-import * as Cities from "./Cities.json";
-import * as Features from "./features.json";
+// import * as Genders from "./genders.json";
+// import * as Cities from "./Cities.json";
+// import * as Features from "./features.json";
 
-const { genders } = Genders;
-const { cities } = Cities;
-const { features } = Features;
+// const { genders } = Genders;
+// const { cities } = Cities;
+// const { features } = Features;
+import cities from "./cities";
+import features from "./features";
+import genders from "./genders";
 
 export { features, genders, cities };
