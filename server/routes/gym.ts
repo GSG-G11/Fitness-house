c996bc3ecaf4b012c90c9646d0012d56dae0c122
rawaneyad <rawaneyad20@gmail.com>
2022-05-22 16:54:10 +0300

    feat: add delete router

--- server/routes/gym.ts
+++ server/routes/gym.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { postImage } from '../controllers';
+import { postImage, deleteImageController } from '../controllers';
 import checkAuth from '../middleware';
 
 const gym = Router();
 
 gym.post('/images', checkAuth('gym'), postImage);
+gym.delete('/images/:id', checkAuth('gym'), deleteImageController);
 
 export default gym;
