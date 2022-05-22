4094375c9b90614110d142758fd72c641aba7962
rawaneyad <rawaneyad20@gmail.com>
2022-05-22 11:07:19 +0300

    fix: remove commented lines

--- client/src/Services/index.js
+++ client/src/Services/index.js
@@ -1,10 +1,3 @@
-// import * as Genders from "./genders.json";
-// import * as Cities from "./Cities.json";
-// import * as Features from "./features.json";
-
-// const { genders } = Genders;
-// const { cities } = Cities;
-// const { features } = Features;
 import cities from "./cities";
 import features from "./features";
 import genders from "./genders";
