be710bba2de60e12c89a3c48316fdf8d69a75d8f
afahgan <afahgan@students.iugaza.edu.ps>
2022-05-24 15:04:21 +0300

    fix: update

--- server/controllers/gyms/addReview.ts
+++ server/controllers/gyms/addReview.ts
@@ -11,22 +11,22 @@ export default async function addReview(req: Request, res: Response, next: NextF
       description,
       userPhone,
     } = await addReviewSchema.validateAsync(req.body, { abortEarly: false });
-    const review = await Review.create({
-      gymId,
-      username,
-      rate,
-      description,
-    });
     const isExist: any = await Subscription.findOne({
       where: { userPhone, gymId },
     });
     if (!isExist) {
       throw new CustomError('عذراً لا يوجد لديك إشتراك في هذا النادي لتممكن من إضافة تقييم', 401);
     }
-    const isActive: any = isExist.status;
-    if (!isActive) {
+    const { status } = isExist;
+    if (!status) {
       throw new CustomError('عذرا لا يوجد عضوية مفعلة', 403);
     }
+    const review = await Review.create({
+      gymId,
+      username,
+      rate,
+      description,
+    });
     res.status(201).json({ message: 'تم إضافة تقييمك بنجاح', review });
   } catch (error: any) {
     if (error.name === 'ValidationError') {
