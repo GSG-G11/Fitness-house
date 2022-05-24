7c8be363242b50eef9c75084d9ddbd0a9768d822
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-05-24 12:23:57 +0300

    feat: update function

--- server/utils/sms.ts
+++ server/utils/sms.ts
@@ -1,20 +1,16 @@
 import twilio from 'twilio';
-import CustomError from './CustomError';
+// import CustomError from './CustomError';
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env;
 
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-const sendSMS = async (to: string, body: string) => {
-  try {
-    await client.messages.create({
-      body,
-      to,
-      from: '+18645236605',
-    });
-  } catch (err: any) {
-    throw new CustomError('عذرا هناك خطأ', 500);
-  }
+const sendSMS = (to: string, body: string) => {
+  client.messages.create({
+    body,
+    to,
+    from: '+18645236605',
+  });
 };
 
 export default sendSMS;
