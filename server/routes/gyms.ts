886e6e661f0aeea6186edb0ee71b5bb4458b9437
afahgan <afahgan@students.iugaza.edu.ps>
2022-05-24 14:15:38 +0300

    fix: move review route

--- server/routes/gyms.ts
+++ server/routes/gyms.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import checkAuth from '../middleware/checkAuth';
-import { getTopGyms, searchGymByName, getGym, getFilteredGyms, gymLogin, gymRegister, editGymData, addReview } from '../controllers';
+import { getTopGyms, searchGymByName, getGym, getFilteredGyms, gymLogin, gymRegister, editGymData } from '../controllers';
 
 const gyms = Router();
 gyms.post('/login', gymLogin);
@@ -12,8 +12,6 @@ gyms.post('/register', gymRegister);
 gyms.get('/top', getTopGyms);
 gyms.get('/filter', getFilteredGyms);
 gyms.get('/search', searchGymByName);
-gyms.post('/reviews', addReview);
 gyms.put('/', checkAuth('gym'), editGymData);
 gyms.get('/:id', getGym);
-
 export default gyms;
