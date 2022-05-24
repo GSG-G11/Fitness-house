b1b18e59567b0c6065c0f3d2e1803f650d6f00d9
rawaneyad <rawaneyad20@gmail.com>
2022-05-24 10:30:44 +0300

    feat: add SubscriberGym component

---
+++ client/src/Components/DashboardPage/Subscriptions/index.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TablePagination,
+  TableRow,
+  Paper,
+} from "@mui/material";
+
+import { useSelector } from "react-redux";
+import EnhancedTableHead from "./EnhancedTableHead";
+import EnhancedTableToolbar from "./EnhancedTableToolbar";
+import EnhancedTableRow from "./EnhancedTableRow";
+
+export default function SubscriberGym() {
+  const rows = useSelector(
+    (state) =>
+      state.subscriptions.queries["getSubscriptions(undefined)"].data
+        .gymSubscription
+  );
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [search, setSearch] = useState("");
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mt: 2.5 }}>
+        <EnhancedTableToolbar setSearch={setSearch} />
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <EnhancedTableHead rowCount={rows.length} />
+            <TableBody>
+              {search
+                ? rows
+                    .filter((row) => row.username.includes(search))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                      return <EnhancedTableRow key={row.userPhone} row={row} />;
+                    })
+                : rows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                      return <EnhancedTableRow key={row.userPhone} row={row} />;
+                    })}
+              {emptyRows > 0 && (
+                <TableRow>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelDisplayedRows={({ from, to, count }) =>
+            ` ${from}-${to} من ${count}`
+          }
+          labelRowsPerPage="عدد الصفوف في الصفحة"
+        />
+      </Paper>
+    </Box>
+  );
+}
