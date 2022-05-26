af05f9b1c246a3e89e6ce98949bb1205f4c18678
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-05-26 22:33:09 +0300

    feat: create reqest for reset password

--- client/src/Pages/RestPassword.jsx
+++ client/src/Pages/RestPassword.jsx
@@ -1,29 +1,79 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import { Typography, Snackbar, Alert } from "@mui/material";
+import { useDispatch } from "react-redux";
 import RestPasswordCom from "../Components/LoginPage/RestPassword";
+import { setAuth } from "../Store/Slices";
 
 export default function RestPassword() {
+  const [isLoading, setIsLoading] = useState(false);
   const [alertMessage, setAlertMessage] = useState({
     message: "",
     open: false,
     vertical: "top",
     horizontal: "center",
+    severity: "info",
   });
 
-  const { message, vertical, horizontal, open } = alertMessage;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const handleClose = (event, reason) => {
+  const { message, vertical, horizontal, open, severity } = alertMessage;
+
+  const handleClose = (_, reason) => {
     if (reason === "clickaway") {
       return;
     }
     setAlertMessage({ ...alertMessage, open: false });
   };
 
+  const onFinish = async (values) => {
+    try {
+      setIsLoading(true);
+      const {
+        data: { payload, responseMessage },
+      } = await axios({
+        method: "POST",
+        url: "/api/v1/gyms/reset/password",
+        data: values,
+      });
+
+      setAlertMessage({
+        ...alertMessage,
+        open: true,
+        message: responseMessage,
+        severity: "success",
+      });
+
+      const { id, name } = payload;
+
+      dispatch(
+        setAuth({
+          id,
+          name,
+          role: "gym",
+          isLoggedIn: true,
+        })
+      );
+      setIsLoading(false);
+
+      navigate("/dashboard/gyms", { replace: true });
+    } catch (error) {
+      setIsLoading(false);
+      setAlertMessage({
+        ...alertMessage,
+        open: true,
+        message: "عذرا حدث خطأ ما , يرجى المحاولة مرة أخرى",
+        severity: "error",
+      });
+    }
+  };
+
   return (
     <div className="container">
       <div className="sub__container">
-        <RestPasswordCom />
+        <RestPasswordCom onFinish={onFinish} isLoading={isLoading} />
         <Typography className="center" component="h4" sx={{ mt: 2 }}>
           ليس لديك حساب ؟&ensp;<Link to="/gym/register"> إنشاء حساب </Link>
         </Typography>
@@ -33,7 +83,7 @@ export default function RestPassword() {
           autoHideDuration={4000}
           onClose={handleClose}
         >
-          <Alert onClose={handleClose} variant="filled" severity="error">
+          <Alert onClose={handleClose} variant="filled" severity={severity}>
             {message}!
           </Alert>
         </Snackbar>
