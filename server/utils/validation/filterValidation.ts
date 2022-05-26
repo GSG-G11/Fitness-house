966bb1fa852980e0088000120919f0fef2af4a69
afahgan <afahgan@students.iugaza.edu.ps>
2022-05-26 11:02:02 +0300

    fix: final fix

--- server/utils/validation/filterValidation.ts
+++ server/utils/validation/filterValidation.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const filterValidation = Joi.object({
   minPrice: Joi.number().integer(),
-  maxPrice: Joi.number().positive().required(),
+  maxPrice: Joi.number().positive().integer(),
   page: Joi.number().integer().positive(),
 });
 
