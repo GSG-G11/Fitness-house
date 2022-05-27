e6ab6846d912babef9a873f8ff66fea62d3e2b62
rawaneyad <rawaneyad20@gmail.com>
2022-05-27 17:29:02 +0300

    fix: add success in severity

--- client/src/Pages/ForgetPassword.jsx
+++ client/src/Pages/ForgetPassword.jsx
@@ -11,7 +11,7 @@ export default function ForgetPassword() {
     open: false,
     vertical: "top",
     horizontal: "center",
-    severity: "",
+    severity: "success",
   });
 
   const { message, vertical, horizontal, open, severity } = alertMessage;
